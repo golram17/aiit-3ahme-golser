4cebf2222442ffac55dc659e5ef7f9c0b4cf37ff
golram17 <37364230+golram17@users.noreply.github.com>
2020-04-27 13:46:37 +0200

    Add files via upload

---
+++ javaProgramme/ue02_serienschaltung/Serienschaltung.java
@@ -0,0 +1,48 @@
+package ue02_serienschaltung;
+
+/**
+ *
+ * @author raphael
+ */
+public class Serienschaltung {
+    private double strom;
+    private double spannung;
+    private double[] widerstandsFeld;
+    
+    public void addWiderstand(double widerstandInOhm){
+            
+            widerstandInOhm = this.widerstandsFeld.length;
+        
+            Serienschaltung spannungBerechnen = new Serienschaltung();
+        
+            spannungBerechnen.updateSpannung();
+            
+           
+        
+    }
+
+    public double getStrom () {
+        return strom;
+    }
+
+    public double getSpannung () {
+        return spannung;
+    }
+
+    public void setStrom (double strom) {
+        this.strom = strom;
+    }
+
+    @Override
+    public String toString () {
+        return "Serienschaltung{" + "strom=" + strom + ", spannung=" + spannung + ", widerstandsFeld=" + widerstandsFeld + '}';
+    }
+    
+   
+    
+    private void updateSpannung(){
+        this.spannung = this.strom * this.widerstandsFeld.length;
+        
+    }
+    
+}
