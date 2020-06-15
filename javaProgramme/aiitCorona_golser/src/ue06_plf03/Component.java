87dcc1eaa870ac527e73e949b752ac200315c15f
golram17 <37364230+golram17@users.noreply.github.com>
2020-06-15 12:43:00 +0200

    Add files via upload

---
+++ javaProgramme/aiitCorona_golser/src/ue06_plf03/Component.java
@@ -0,0 +1,14 @@
+package ue06_plf03;
+
+/**
+ *
+ * @author raphael
+ */
+public class Component {
+    private String id;
+    private double value;
+    private double voltage;
+    private double current;
+    
+    
+}
